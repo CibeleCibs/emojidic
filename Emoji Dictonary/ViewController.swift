e56d3033de504955bc46048c2d6a179ddc876105
Cibele Bustamante <cibelebustamante@gmail.com>
2017-03-22 23:08:23 -0300

    Initial Commit

---
+++ Emoji Dictonary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictonary
+//
+//  Created by Cibele Bustamante on 22/03/17.
+//  Copyright © 2017 Cibele Bustamante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
